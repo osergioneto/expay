ceb6aae09d23de815221bb9287ad80803a5db3e3
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-04-16 02:51:13 -0300

    feat: add changeset function to user and account schema

--- lib/expay/accounts/schemas/account.ex
+++ lib/expay/accounts/schemas/account.ex
@@ -3,12 +3,16 @@ defmodule Expay.Accounts.Schemas.Account do
   Account of a user.
   """
   use Ecto.Schema
+
+  import Ecto.Changeset
+
   alias Expay.Users.Schemas.User
 
   @required [:balance, :number, :agency, :user_id]
-
+  @optional []
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
+
   schema "accounts" do
     field :balance, :integer
     field :number, :string
@@ -16,5 +20,14 @@ defmodule Expay.Accounts.Schemas.Account do
     belongs_to :user, User
 
     timestamps()
+  end
+
+  # TODO: Add validation on number and agency
+  def changeset(model \\ %__MODULE__{}, params) do
+    model
+    |> cast(params, @required ++ @optional)
+    |> validate_required(@required)
+    |> unique_constraint(:number)
+    |> check_constraint(:balance, name: :balance_must_be_positive)
   end
 end
--- lib/expay/users/schemas/user.ex
+++ lib/expay/users/schemas/user.ex
@@ -4,16 +4,41 @@ defmodule Expay.Users.Schemas.User do
   """
   use Ecto.Schema
 
-  @required [:name, :email, :document_number]
+  import Ecto.Changeset
+  import Expay.Changesets
 
+  alias Expay.Users.Schemas.Password
+  alias Expay.Accounts.Schemas.Account
+
+  @required [:name, :email, :document_number]
+  @optional []
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
+
   schema "users" do
     field :name, :string
     field :email, :string
     field :document_number, :string
-    has_one :password, Expay.Users.Schemas.Password
+    has_one :password, Password
+    has_one :account, Account
 
     timestamps()
   end
+
+  def changeset(model \\ %__MODULE__{}, params) do
+    model
+    |> cast(params, @required)
+    |> validate_required(@required)
+    |> validate_length(:name, min: 2)
+    |> validate_email(:email)
+    |> unique_constraint(:email)
+    |> unique_constraint(:document_number)
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Bcrypt.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
