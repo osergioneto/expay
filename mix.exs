ed01012eb24be01290310652a4f0de10bdc50c48
Sérgio D. Neto <sergio.deusdedith@gmail.com>
2021-04-16 04:13:44 -0300

    chore: add bcrypt elixir

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Expay.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:bcrypt_elixir, "~> 2.0"},
     ]
   end
 
